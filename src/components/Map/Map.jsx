e7b5596cdee0083736d2a83aaf0159c95f24c1fc
Hang Nguyen <thuhang.nguyen0101@gmail.com>
2022-10-25 15:28:05 -0700

    google map is implemented

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,4 +1,4 @@
-import { GoogleMap, LoadScript } from '@react-google-maps/api'
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
 import {Paper, Typography, useMediaQuery} from '@mui/material';
 import LocationOnOutlinedIcon from '@mui/icons-material/LocationOnOutlined';
 import Rating from '@mui/material/Rating';
@@ -6,6 +6,11 @@ import './Map.css';
 
 export default function Map() {
     const isMobile = useMediaQuery('(min-width: 600px)');
+
+    const containerStyle = {
+        width: '100%',
+        height: '85vh',
+      };
 
     const coordinates = {
         lat: 0,
@@ -15,10 +20,10 @@ export default function Map() {
         <div className='map-container'>
             <LoadScript googleMapsApiKey='AIzaSyD3Kbavz-Q5_BJdFiLJ8qOOW_mX3UcNtsI'>
                 <GoogleMap
-                    defaultCenter={coordinates}
                     center={coordinates}
-                    defaultZoom={14}
+                    zoom={10}
                     margin={[50, 50, 50, 50]}
+                    mapContainerStyle={containerStyle}
                     // options={''}
                     // onChange={''}
                     // onChildClick={''}
