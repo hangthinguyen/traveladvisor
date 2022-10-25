f789ecae77f4543693521b5638b96a670387d144
Hang Nguyen <thuhang.nguyen0101@gmail.com>
2022-10-25 14:54:18 -0700

    using api to access google map

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,13 +1,30 @@
-import GoogleMapReact from 'google-map-react';
+import { GoogleMap, LoadScript } from '@react-google-maps/api'
 import {Paper, Typography, useMediaQuery} from '@mui/material';
 import LocationOnOutlinedIcon from '@mui/icons-material/LocationOnOutlined';
-import Rating from '@mui/lab/Rating'
+import Rating from '@mui/material/Rating';
 import './Map.css';
 
 export default function Map() {
+    const isMobile = useMediaQuery('(min-width: 600px)');
+
+    const coordinates = {
+        lat: 0,
+        lng: 0,
+    }
     return (
-        <div>
-            Hello
+        <div className='map-container'>
+            <LoadScript googleMapsApiKey='AIzaSyD3Kbavz-Q5_BJdFiLJ8qOOW_mX3UcNtsI'>
+                <GoogleMap
+                    defaultCenter={coordinates}
+                    center={coordinates}
+                    defaultZoom={14}
+                    margin={[50, 50, 50, 50]}
+                    // options={''}
+                    // onChange={''}
+                    // onChildClick={''}
+                >
+                </GoogleMap>
+            </LoadScript>
         </div>
     )
 }
